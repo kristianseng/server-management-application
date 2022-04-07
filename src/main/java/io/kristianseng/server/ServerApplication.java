1c5bace4dc334b73e1860383f6667f07575ee1b8
krsitianse <kristians.engels@balcia.com>
2022-04-07 18:30:29 +0300

    server APIs

--- src/main/java/io/kristianseng/server/ServerApplication.java
+++ src/main/java/io/kristianseng/server/ServerApplication.java
@@ -1,7 +1,11 @@
 package io.kristianseng.server;
 
+import io.kristianseng.server.model.Server;
+import io.kristianseng.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +14,14 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server());
+			serverRepo.save(new Server());
+			serverRepo.save(new Server());
+			serverRepo.save(new Server());
+		};
+	}
 }
